470d3faed0a290989ad25adc849db7b178f02709
suman kar <enter>
2025-06-25 14:09:18 +0530

    update

--- ARRYAS/array with user input.c
+++ ARRYAS/array with user input.c
@@ -1,13 +1,13 @@
+
 #include<stdio.h>
-int main(){ 
+int main(){
     int arr[5];
-    for(int i=0; i<=4;i++){
-        int a= i+1;
-printf("Enter the number of %d element : ",i);
+    for(int i=0; i<5;i++){
+        printf("Enter the number of %d element : ",i+1);
         scanf("%d",&arr[i]);
     }
     for(int i=0; i<5;i++){
-    printf("%d ",arr[i]);
+        printf("%d ",arr[i]);
     }
     return 0;
 }
