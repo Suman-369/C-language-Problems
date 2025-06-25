d10eae8daf99722841ad45b6d0ed9935c918363f
suman kar <enter>
2025-06-25 14:15:58 +0530

    update

--- ARRYAS/functionarray.c
+++ ARRYAS/functionarray.c
@@ -1,9 +1,9 @@
+
 #include<stdio.h>
 void fun( int arr[]){
     arr[0]= 10;
     return;
 }
-
 int main(){
     int arr[5]= {1,2,3,4,5};
     printf("%d\n",arr[0]);
@@ -11,3 +11,6 @@ int main(){
     printf("%d\n",arr[0]);
     return 0;
 }
+
+// Output:
+// 1
